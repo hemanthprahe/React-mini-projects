b5e08f9b352a1f8dcf043366c78d24822f0d2306
KAARTHIIK M <kaarthiikmr001@gmail.com>
2024-02-27 21:26:07 +0530

    test display name

---
+++ XDisplayName/xdisplayname/src/Name.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Name = () => {
+  return (
+    <div>
+        <h1>Full Name Display</h1>
+        <p>First Name:</p>
+        <p>Last Name:</p>
+    </div>
+  )
+}
+
+export default Name
